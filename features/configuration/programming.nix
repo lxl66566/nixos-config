fcf5563cb7229032fc265bcbb742ad7084a68ffa
lxl66566 <lxl66566@gmail.com>
2025-11-07 20:03:41 +0800

    update

--- features/configuration/programming.nix
+++ features/configuration/programming.nix
@@ -63,6 +63,8 @@
         libarchive
         stylua
         autocorrect
+        nftables
+        net-tools
       ]
       ++ (lib.optionals (features.desktop != [ ] && !features.wsl) [
         androidStudioPackages.dev
