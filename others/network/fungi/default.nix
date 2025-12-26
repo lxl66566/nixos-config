8ee70e2a87420710232527454ff7df74556372ca
lxl66566 <lxl66566@gmail.com>
2025-12-26 21:12:25 +0800

    update tcp params

--- others/network/fungi/default.nix
+++ others/network/fungi/default.nix
@@ -5,9 +5,7 @@
   ...
 }:
 {
-  imports = [
-    pkgs.nur.repos.lxl66566.modules.fungi
-  ];
+  # before using this module, please import `pkgs.nur.repos.lxl66566.nixosModules.fungi` in your flake.nix
   services.fungi = {
     enable = true;
     configFile = ./config.toml;
